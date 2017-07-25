32d6b631e66419a26f54b433960d3cdd29f54126
tanwu <tanwu@duocai100.com>
2017-07-25 17:53:43 +0800

    update

--- docs/es6/7_Function_expand.js
+++ docs/es6/7_Function_expand.js
@@ -49,33 +49,74 @@
 //console.log(foo());
 
 //与解构赋值默认值结合使用
+//function foo({x, y=5}){
+//    console.log(x, y);
+//}
+////console.log(foo({}));
+////console.log(foo({x: 1}));
+////console.log(foo({x: 1, y: 2}));
+//console.log(foo());
 
+//function fetch(url, {body='', method='GET', headers={}}){
+//    console.log(method);
+//}
+////console.log(fetch('http://www.baidu.com', {}));
+//console.log(fetch('http://www.baidu.com'));
 
+//结合函数参数的默认值，就可以省略第二个参数，就出现了双重默认值
+//function fetch(url, {method='GET'} = {}){
+//    console.log(method);
+//}
+//fetch('http://www.baidu.com');
 
+//两种写法的区别？
+//function m1({x=0, y=0} = {}){
+//    return [x, y];
+//}
+//function m2({x, y} = {x: 0, y: 0}){
+//    return [x, y];
+//}
+//console.log(m1());
+//console.log(m2());
 
+//参数默认值的位置
+//通常情况下，定义了默认值的参数，应该是函数的尾参数。因为这样比较容易看出来，到底省略了哪些参数。如果非尾部的参数设置默认值，实际上这个参数是没法省略的
+//function f(x=1, y){
+//    return [x, y];
+//}
+////console.log(f());
+////console.log(f(1));
+////console.log(f(, 1));
+//console.log(f(undefined, 1));
 
+//function f(x, y=5, z){
+//    return [x, y, z];
+//}
+////console.log(f());
+////console.log(f(1));
+////console.log(f(1 , , 1));
+//console.log(f(1, undefined, 2));
 
+//如果传入undefined，将触发该参数等于默认值，null则没有这个效果
+//function foo(x=5, y=6){
+//    console.log(x, y);
+//}
+//console.log(foo(undefined, null));
 
+//函数的 length 属性
+//指定了默认值以后，函数的length属性，将返回没有指定默认值的参数个数。也就是说，指定了默认值后，length属性将失真
+//console.log((function (a){}).length);
+//console.log((function (a=5){}).length);
+//console.log((function (a,b,c=5){}).length);
 
+//rest
+//console.log((function (...args){}).length);
 
+//如果设置了默认值的参数不是尾参数，那么length属性也不再计入后面的参数了
+//console.log((function(a=0, b, c){}).length);
+//console.log((function(a, b=1, c){}).length);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//作用域
 
 
 
